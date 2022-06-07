a1fa2834a8401dd6d6f36c4b2d98247e2151813a
Bashing <bashing@outlook.de>
2022-06-07 18:58:24 +0200

    Player Movementanimation added

--- Player/Player.cs
+++ Player/Player.cs
@@ -7,46 +7,45 @@ using static Godot.GD;
 public class Player : KinematicBody2D
 {
 
-	[Export] int SPEED = 10;
-	[Export] int FRICTION = 25;
-	[Export] int ACCELERATION = 8;
-	Vector2 velocity = Vector2.Zero;
+    [Export] int SPEED = 10;
+    [Export] int FRICTION = 25;
+    [Export] int ACCELERATION = 8;
+    Vector2 velocity = Vector2.Zero;
 
 
-	public AnimationPlayer animationPlayer;
-	public override void _Ready()
-	{
-		animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-	}
-	public override void _PhysicsProcess(float delta)
-	{
-		var input_vector = Vector2.Zero;
+    public AnimationPlayer animationPlayer;
+    public AnimationTree animationTree;
+    private AnimationNodeStateMachinePlayback animationState;
 
-		input_vector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
-		input_vector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
-		input_vector = input_vector.Normalized() * SPEED;
+    public override void _Ready()
+    {
+        animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        animationTree = GetNode<AnimationTree>("AnimationTree");
+		animationState = (AnimationNodeStateMachinePlayback)animationTree.Get("parameters/playback");
+    }
+    public override void _PhysicsProcess(float delta)
+    {
+        var input_vector = Vector2.Zero;		
 
-		if (input_vector != Vector2.Zero)
-		{
-			if (input_vector.x > 0)
-			{
-				animationPlayer.Play("RunRight");
-			}
-			else
-			{
-				animationPlayer.Play("RunLeft");
-			}
-			// velocity = velocity.MoveToward(input_vector * SPEED, ACCELERATION) * (1 + delta);
-			velocity = velocity.MoveToward(input_vector * SPEED, ACCELERATION * (1 + delta));
-		}
-		else
-		{
-			animationPlayer.Play("IdleRight");
-			// velocity = velocity.MoveToward(Vector2.Zero, FRICTION) * (1 + delta);
-			velocity = velocity.MoveToward(Vector2.Zero, FRICTION * (1 + delta));
-		}
+        input_vector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+        input_vector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
+		
+        input_vector = input_vector.Normalized() * SPEED;
 
-		velocity = MoveAndSlide(velocity);
-	}
+        if (input_vector != Vector2.Zero)
+        {
+            animationTree.Set("parameters/Idle/blend_position", input_vector);
+            animationTree.Set("parameters/Run/blend_position", input_vector);
+            animationState.Travel("Run");
 
+            velocity = velocity.MoveToward(input_vector * SPEED, ACCELERATION * (1 + delta));
+        }
+        else
+        {
+            animationState.Travel("Idle");
+
+            velocity = velocity.MoveToward(Vector2.Zero, FRICTION * (1 + delta));
+        }
+        velocity = MoveAndSlide(velocity);
+    }
 }
